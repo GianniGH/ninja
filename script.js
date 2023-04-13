0ed30708fb6e9ef67d397668d6d58e45ad67c3ee
MumuG <79053287+GianniGH@users.noreply.github.com>
2023-04-13 13:52:14 -0400

    [tarea] Sensei

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ ninja1.sayName()
 ninja1.showStats(ninja1)
 ninja1.drinkSake()
 console.log(ninja1.health)
+
+class Sensei extends Ninja {
+    constructor(name,wisdom) {
+        super(name, 200, 10, 10, wisdom)
+        this.wisdom = 10
+    }
+    speakWisdom() {
+        this.drinkSake()
+        console.log(
+        'Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses'
+        )
+    }
+}
+
+sensei1 = new Sensei('Juanin')
+console.log(sensei1)
+sensei1.speakWisdom()
+sensei1.showStats(sensei1)
